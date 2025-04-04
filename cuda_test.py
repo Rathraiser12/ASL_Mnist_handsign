7bcad750d2de024036c62aef6692ad7b580f1503
Rathan Ramesh <rathr0205@gmail.com>
2025-04-04 14:30:41 +0200

    Included the collect script

--- cuda_test.py
+++ cuda_test.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 
 gpus = tf.config.list_physical_devices('GPU')
 if gpus:
-    print("✅ GPU detected. Using CUDA.")
+    print("GPU detected. Using CUDA.")
     for gpu in gpus:
         print("GPU:", gpu)
 else:
-    print("❌ No GPU detected. Running on CPU.")
+    print("No GPU detected. Running on CPU.")
